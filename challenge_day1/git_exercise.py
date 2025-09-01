e68c91ab2037cc45cb7281e50144c1fe3517e451
karolinavisan <karolina.visan@student.uva.nl>
2025-09-01 11:16:56 +0200

    ok

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -6,15 +6,8 @@ def team_intro():
     print(f"This is team {team_name}, we are:")
     print(isar.name())
     print(david.david())
-<<<<<<< HEAD
     print(karolina.karolina())
-=======
     print(norina.norina())
-<<<<<<< HEAD
->>>>>>> f8193270e82b96f553e9790555cd46d5b3439d5a
-=======
-    print(pija.name())
-    print(karolina.karolina())
->>>>>>> 96566cb08321f90310fedd6101ddc9edf83d780b
+
 
 team_intro()
