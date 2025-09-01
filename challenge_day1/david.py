c27855fb0c347426a10c37bffb0b2dc02b9ce36a
davidguarinv <davidguarinv@gmail.com>
2025-09-01 11:20:28 +0200

    david's story

--- challenge_day1/david.py
+++ challenge_day1/david.py
@@ -2,3 +2,13 @@ def david ():
     return ("David")
     
 david()
+
+def act1():
+    return "Meanwhile, Oliver, a mischievious stray dog, ran circles around the group, barking at shaddows. He seemed to know somthing about the town, as if he had a secret misson of his own. Lila laughed, deciding he would be thier unofficial pet guid."
+
+def act2():
+    return "Marcus tried to take a closer look but slippd, nearly falling into a hidden pit. Jace grabbed his arm, shouting, “Carefull! This place is a death trap!” Tessa scribbled furiously in her papers, trying to make sence of the chaotic scene."
+
+def act3():
+    return "Together, the six friends worked quickly, adjusting gears, pulling levers, and aligning strange inscripshions. Sweat dripped down thier faces as the clocktower seemed to come alife. Jace, surprisingly, whispered, “Okay… this is actualy kinda cool.”"
+
