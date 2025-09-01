118258aea891ca1b49bedcc3db4da40bb5cb0f00
Isar-K <isar.kerkvliet@gmail.com>
2025-09-01 11:29:17 +0200

    spelling fixed david
    fixed everything

--- challenge_day1/david.py
+++ challenge_day1/david.py
@@ -4,10 +4,10 @@ def david ():
 david()
 
 def act1():
-    return "Meanwhile, Oliver, a mischievious stray dog, ran circles around the group, barking at shaddows. He seemed to know somthing about the town, as if he had a secret misson of his own. Lila laughed, deciding he would be thier unofficial pet guid."
+    return "Meanwhile, Oliver, a mischievous stray dog, ran circles around the group, barking at shadows. He seemed to know something about the town, as if he had a secret mission of his own. Lila laughed, deciding he would be their unofficial pet guide."
 
 def act2():
-    return "Marcus tried to take a closer look but slippd, nearly falling into a hidden pit. Jace grabbed his arm, shouting, “Carefull! This place is a death trap!” Tessa scribbled furiously in her papers, trying to make sence of the chaotic scene."
+    return "Marcus tried to take a closer look but slipped, nearly falling into a hidden pit. Jace grabbed his arm, shouting, “Careful! This place is a death trap!” Tessa scribbled furiously in her papers, trying to make sense of the chaotic scene."
 
 def act3():
-    return "Together, the six friends worked quickly, adjusting gears, pulling levers, and aligning strange inscripshions. Sweat dripped down thier faces as the clocktower seemed to come alife. Jace, surprisingly, whispered, “Okay… this is actualy kinda cool.”"
+    return "Together, the six friends worked quickly, adjusting gears, pulling levers, and aligning strange inscriptions. Sweat dripped down their faces as the clocktower seemed to come alive. Jace, surprisingly, whispered, “Okay… this is actually kinda cool.”"
