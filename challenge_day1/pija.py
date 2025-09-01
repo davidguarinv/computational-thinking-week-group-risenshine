00ba36eff63590bd7a3d4b22e437124a450f79c7
pija-chm <pija.chm@gmail.com>
2025-09-01 11:19:08 +0200

    pija paragraphs

--- challenge_day1/pija.py
+++ challenge_day1/pija.py
@@ -1,2 +1,11 @@
 def name():
     return "Pija"
+
+def act1():
+    return "Lila, a curios teen, steped into the old town for the frist time, her eyes wide with wondr. She had herd storis of the clocktower that never tolled on tiem. Beside her, her brother Jace was allreddy skeptical, muttering about wasted time."
+
+def act2():
+    return "Inside the tower, the wooden stairs creaked under thier weight, each step echoeing through the emptie halls. Lila’s flashlight flickered, casting spooky shaps on the walls. Marcus mumbled about old hauntings, making Jace scff loudly."
+
+def act3():
+    return "Back on the tower’s balcomy, the town stretched out below them, lights twinkeling like stars. Lila realized the clocktower wasn’t just a building; it was a gaurdian of hidden knowlege. Jace grumbled, “All I see is a high plase to get blown off."
