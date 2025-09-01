e9b9884ff1c9acdcdb029e92d1f2b60c40e04440
karolinavisan <karolina.visan@student.uva.nl>
2025-09-01 11:27:01 +0200

    my parts

--- challenge_day1/karolina.py
+++ challenge_day1/karolina.py
@@ -2,3 +2,10 @@ def karolina():
     return "Karolina"
 
 karolina()
+
+def act1():
+    return "Marcus, the local shopkeepr, waved at them from across the streat. “Ye’ll need a guid if ye want to find the tower!” he shouted, spillng some of his fresh baked breadds. Lila nodded excitedly, her notebook ready to scrible down every detale."
+def act2():
+    return "Tessa pointed out a serie of strange symbols carved into the stone walls. “These are older then the town itself,” she said, brushing away dust. Lila’s heart raced as she tried to decifer them, feeling like they held a key to somthing bigger."
+def act3():
+    return "Tessa carefully picked up one of the manuscripts, brushing off the dust. “It speeks of a timekeeper… someone who can control the flow of hours,” she whispered. Marcus scratched his head, muttering about how he prefered baking to time travell."
