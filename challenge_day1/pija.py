585bd46c7d40aeb282cf9550d0d99cf02b66ee93
karolinavisan <karolina.visan@student.uva.nl>
2025-09-01 11:36:15 +0200

    changed pijas part

--- challenge_day1/pija.py
+++ challenge_day1/pija.py
@@ -2,10 +2,10 @@ def name():
     return "Pija"
 
 def act1():
-    return "Lila, a curios teen, steped into the old town for the frist time, her eyes wide with wondr. She had herd storis of the clocktower that never tolled on tiem. Beside her, her brother Jace was allreddy skeptical, muttering about wasted time."
+    return "Lila, a curious teen, stepped into the old town for the first time, her eyes wide with wonder. She had heard stories of the clocktower that never tolled on time. Beside her, her brother Jace was already skeptical, muttering about wasted time."
 
 def act2():
-    return "Inside the tower, the wooden stairs creaked under thier weight, each step echoeing through the emptie halls. Lila’s flashlight flickered, casting spooky shaps on the walls. Marcus mumbled about old hauntings, making Jace scff loudly."
+    return "Inside the tower, the wooden stairs creaked under their weight, each step echoing through the empty halls. Lila’s flashlight flickered, casting spooky shapes on the walls. Marcus mumbled about old hauntings, making Jace scuff loudly."
 
 def act3():
-    return "Back on the tower’s balcomy, the town stretched out below them, lights twinkeling like stars. Lila realized the clocktower wasn’t just a building; it was a gaurdian of hidden knowlege. Jace grumbled, “All I see is a high plase to get blown off."
+    return "Back on the tower’s balcony, the town stretched out below them, lights twinkling like stars. Lila realized the clocktower wasn’t just a building; it was a guardian of hidden knowledge. Jace grumbled, “All I see is a high place to get blown off.”"
