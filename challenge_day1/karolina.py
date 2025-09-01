32a85d11bdd923236736bafdc5db0a9ed476b91b
davidguarinv <davidguarinv@gmail.com>
2025-09-01 11:30:45 +0200

    spelling karolina

--- challenge_day1/karolina.py
+++ challenge_day1/karolina.py
@@ -4,7 +4,7 @@ def karolina():
 karolina()
 
 def act1():
-    return "Marcus, the local shopkeepr, waved at them from across the streat. “Ye’ll need a guid if ye want to find the tower!” he shouted, spillng some of his fresh baked breadds. Lila nodded excitedly, her notebook ready to scrible down every detale."
+    return "Marcus, the local shopkeeper, waved at them from across the streat. “Ye’ll need a guid if ye want to find the tower!” he shouted, spillng some of his fresh baked breads. Lila nodded excitedly, her notebook ready to scrible down every detale."
 def act2():
     return "Tessa pointed out a serie of strange symbols carved into the stone walls. “These are older then the town itself,” she said, brushing away dust. Lila’s heart raced as she tried to decifer them, feeling like they held a key to somthing bigger."
 def act3():
