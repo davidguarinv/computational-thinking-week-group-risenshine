bca24c4b2450fea771ef85b5fc5628828c0066d7
Isar-K <isar.kerkvliet@gmail.com>
2025-09-01 11:09:31 +0200

    d
    d

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -8,7 +8,12 @@ def team_intro():
     print(david.david())
     print(karolina.karolina())
     print(norina.norina())()
-    print(karolina.karolina())
+    print(pija.name())
 
 
 team_intro()
+
+
+## Below is the bonus challenge 
+## three acts 1 paragraph each person per act, work with a partner and fix spelling mistakes
+
