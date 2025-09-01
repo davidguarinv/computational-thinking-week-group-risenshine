1ece54206cb1c9839d1aa86c0c4744c0c7ba2520
Norina Sayande <norinatouray11@gmail.com>
2025-09-01 10:21:50 +0200

    norinas function

---
+++ norina.py
@@ -0,0 +1,9 @@
+def coolest_person():
+    answer= input("Who is the coolest person in the world?")
+    if answer== "Norina" or "norina":
+        print("Correct! Norina is the coolest person in the world!")
+    else:
+        print("Nope. The only correct answer is Norina.")
+
+
+coolest_person()
