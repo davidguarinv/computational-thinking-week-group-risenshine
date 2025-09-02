cbdaf699f14f3f9c507bc02a155a453253821f76
Norina Sayande <norinatouray11@gmail.com>
2025-09-02 10:09:14 +0200

    station 6

---
+++ challenge_day2/station6.py
@@ -0,0 +1,7 @@
+def solution_station6(x: float) -> float:
+    y = (0.0383238 * x**4
+         - 0.24111 * x**3
+         + 0.070717 * x**2
+         + 0.968811 * x
+         + 0.00482126)
+    return y
